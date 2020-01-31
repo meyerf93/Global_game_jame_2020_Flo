48104a159e4f65e7bb879a2a499682d07a35ca7d
Unknown <florian.meyer@edu.hefr.ch>
2020-02-01 00:31:09 +0100

    correct file new feature input

---
+++ Global_gamejam_2020/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    InputMaster Controls;
+
+    Vector2 move;
+
+    private void Awake()
+    {
+        Controls = new InputMaster();
+        Controls.Player.Fire.performed += _ => shoot();
+        Controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        Controls.Player.Move.canceled += ctx => move = Vector2.zero;
+
+    }
+
+    void shoot()
+    {
+        Debug.Log("fire");
+    }
+
+
+    private void Update()
+    {
+        Vector2 m = new Vector2(move.x, move.y) * Time.deltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+    private void OnEnable()
+    {
+        Controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Controls.Player.Disable();
+    }
+}
