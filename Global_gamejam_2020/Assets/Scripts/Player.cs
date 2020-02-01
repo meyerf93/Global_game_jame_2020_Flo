f9f22f238c53d6dc98ee22cac0852ac77d37258b
Unknown <florian.meyer@edu.hefr.ch>
2020-02-01 08:55:05 +0100

    change the .gitignore

--- Global_gamejam_2020/Assets/Scripts/Player.cs
+++ Global_gamejam_2020/Assets/Scripts/Player.cs
@@ -7,9 +7,18 @@ public class Player : MonoBehaviour
     InputMaster Controls;
 
     Vector2 move;
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector3 m_Velocity = Vector3.zero;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
+
 
     private void Awake()
     {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+
+
         Controls = new InputMaster();
         Controls.Player.Fire.performed += _ => shoot();
         Controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
@@ -25,8 +34,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        Vector2 m = new Vector2(move.x, move.y) * Time.deltaTime;
-        transform.Translate(m, Space.World);
+        Move(move.x, move.y);
     }
 
     private void OnEnable()
@@ -38,4 +46,46 @@ public class Player : MonoBehaviour
     {
         Controls.Player.Disable();
     }
+
+
+
+
+
+
+
+    public void Move(float move_x, float mvoe_y)
+    {
+
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(move_x, mvoe_y);
+        // And then smoothing it out and applying it to the character
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+        // If the input is moving the player right and the player is facing left...
+        if (move_x > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (move_x < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        
+    }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }
---
+++ Global_gamejam_2020/Assets/camera_controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour
+{
+
+    public GameObject player;
+    public BoxCollider2D boundsBox;
+
+    private Camera theCamera;
+
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private float halfheight;
+    private float halfWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minBounds = boundsBox.bounds.min;
+        maxBounds = boundsBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfheight = theCamera.orthographicSize;
+        halfWidth = halfheight * Screen.width / Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
