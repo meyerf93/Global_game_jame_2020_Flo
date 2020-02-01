b5279bbc7cbdb0a4c3f0cb0aa561a8160c44c3fc
Emilie <adamanska@gmail.com>
2020-02-01 19:28:32 +0100

    Added timer, cleaned and PREFABED canvases

---
+++ Global_gamejam_2020/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    public Text textBox;
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+    void Start()
+    {
+        textBox.text = FormatTime(currentTime);
+    }
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        textBox.text = FormatTime(currentTime);
+    }
+}
