f899a81567e8b6eadce561f6c8144f7f8ef2b93a
Thierry <cyphics@protonmail.com>
2020-02-02 15:29:50 +0100

    add path finding and building destruction by monsters

--- Global_gamejam_2020/Assets/Scripts/Logic/Creatures/Monster.cs
+++ Global_gamejam_2020/Assets/Scripts/Logic/Creatures/Monster.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using Logic.World;
 using UnityEngine;
-
+using Pathfinding;
 public class Monster : Creature
 {
     public int amount_of_actions = 4;
     public Building currentTarget;
-
+    public AIPath AIPath;
+    public AIDestinationSetter DestinationSetter;
     private WorldMap map;
-    private void Awake()
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Debug.Log($"Trigger collison with {other}");
+        Destroy(other.gameObject);
     }
 
     public void GoDestroyStuff()
@@ -36,14 +39,18 @@ public class Monster : Creature
         if (buildings.Any())
         {
             
-            Debug.Log("Evil destroy building!");
+            //Debug.Log("Evil destroy building!");
             currentTarget = buildings[0];
-            map.DestroyBuilding(currentTarget);
+            //GoTo(currentTarget);
+            //DestinationSetter.target = currentTarget.transform;
+            //map.DestroyBuilding(currentTarget);
         }
         
         // Go to location
         // Destroy building
     }
+
+     
 
     public void SetWorldMap(WorldMap worldMap)
     {
--- Global_gamejam_2020/Assets/Scripts/Logic/World/Building.cs
+++ Global_gamejam_2020/Assets/Scripts/Logic/World/Building.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    //private BoxCollider2D m_boxCollider2D;
+
+    private void Awake()
+    {
+        //m_boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     Debug.Log("Collision detected");
+    //     if (collision.gameObject.CompareTag("Monster"))
+    //     {
+    //         Debug.Log("Building destroyed");
+    //        Destroy(gameObject);
+    //     }
+    // }
 }
